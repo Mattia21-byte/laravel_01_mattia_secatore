ba9e1c7d3e401c6f0873a72a05f67dedbdbb2f77
Mattia21-byte <mattiago2105@gmail.com>
2026-05-21 11:28:13 +0200

    esercizio views

---
+++ resources/views/pagina2.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Pagina 2</h1>
+    <a href="/">Homepage</a>
+    <a href="/pagina1">pagina1</a>
+</body>
+</html>
